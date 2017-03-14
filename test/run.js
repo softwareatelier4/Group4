7cf9f8dd84f26af19027fcb1967697b1b80a9ea1
Gregory Wullimann <wullimann.gregory@gmail.com>
2017-03-14 14:44:08 +0100

    Creating testing env

---
+++ test/run.js
@@ -0,0 +1,9 @@
+var should = require('should');
+
+describe('Run test environment', function(done){
+
+  describe('Found test script', function(){
+    it('should have a constructor', function(){
+    	done();
+    })
+  });
---
+++ test/test1/run.js
@@ -0,0 +1,10 @@
+var should = require('should');
+var utils =  require('../utils');
+
+describe('Run test environment', function(done){
+
+  describe('Found test script', function(){
+    it('should have a constructor', function(){
+    	done();
+    })
+  });
