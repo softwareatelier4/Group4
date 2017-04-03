1f88c9658d04a1686e800c12da59ef8fb5125e0f
Rebinskiy <g.s.rebinskiy@gmail.com>
2017-04-03 12:04:02 +0200

    Jenkins

--- database/migrations/1481148628058_users.js
+++ database/migrations/1481148628058_users.js
@@ -5,17 +5,17 @@ const Schema = use('Schema')
 class UsersTableSchema extends Schema {
 
   up () {
-    this.create('users', table => {
-      table.increments('id').primary()
-
-      table.string('email').notNullable().unique()
-      table.string('password', 60).notNullable()
-
-      table.string('name').defaultTo('Anonymous')
-      table.enu('type', ['worker', 'user'])
-
-      table.timestamps()
-    })
+    // this.create('users', table => {
+    //   table.increments('id').primary()
+    //
+    //   table.string('email').notNullable().unique()
+    //   table.string('password', 60).notNullable()
+    //
+    //   table.string('name').defaultTo('Anonymous')
+    //   table.enu('type', ['worker', 'user'])
+    //
+    //   table.timestamps()
+    // })
   }
 
   down () {
