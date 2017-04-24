dd0f314c1c5c560fa8df0f7e39c515309137cafd
Gregory Wullimann <wullimann.gregory@gmail.com>
2017-04-25 00:36:23 +0200

    Minor fixes to the authetication

--- app/Http/Middleware/GlobalHelpers.js
+++ app/Http/Middleware/GlobalHelpers.js
@@ -18,17 +18,20 @@ class GlobalHelpers {
     const allCategories = yield this.Category.all()
     view.global('allCategories', allCategories.toJSON())
 
-
     let queryWithoutOrder = '/profiles?'
 
     const components = request.except('orderBy')
 
-    for ( var key in request.except('orderBy') )
-          queryWithoutOrder += `${key}=${decodeURIComponent(components[key])}&`
+    for (var key in request.except('orderBy')) {
+      queryWithoutOrder += `${key}=${decodeURIComponent(components[key])}&`
+    }
 
+    const currentUser = yield request.auth.getUser()
     response.viewInstance = use('View')
+    response.viewInstance.global('currentUser', currentUser)
     response.viewInstance.global('request', request.all())
     response.viewInstance.global('queryWithoutOrder', queryWithoutOrder)
+
     yield next
   }
 
